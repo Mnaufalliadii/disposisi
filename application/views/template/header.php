81c8b1ef57080aa814d58ea954c2e0ef65d1f4b8
Mnaufalliadii <108429599+Mnaufalliadii@users.noreply.github.com>
2022-07-02 15:42:54 +0800

    Membuat surat masuk
    Membuat surat masuk

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -9,5 +9,11 @@
             <title>Sistem Point</title>
             <link rel="stylesheet" href="<?= base_url('assests/css/styles.css')?> " rel="stylesheet"/>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
+                        <!-- Latest compiled and minified CSS -->
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
+            <!-- Latest compiled and minified JavaScript -->
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
+            <!-- (Optional) Latest compiled and minified JavaScript translation files -->
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/i18n/defaults-*.min.js"></script>
         </head>
     </html>
